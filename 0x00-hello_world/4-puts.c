11acfa67c44875455af6a5e37784302a824cf5d0
Ukamaka Egwu <egwuukamaka@gmail.com>
2022-10-14 16:44:33 -0700

    C programming

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * Variables
+ * Main - Entry Point
+ * Return - 0 if true
+ */
+
+int main()
+{
+	puts("Programming is like building a multilingual puzzle\n");
+        return (0);
+}
