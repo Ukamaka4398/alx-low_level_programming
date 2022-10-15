497b9eb0bafa8f076edaf026e31c276d82570a28
Ukamaka Egwu <egwuukamaka@gmail.com>
2022-10-14 19:09:53 -0700

    C programming

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,14 +1,10 @@
 #include <stdio.h>
-
 /**
- *
- * main - entry point
- * Return - Always return 0 if successful
- */
-
-int main()
+* main - entry point
+* Return: Always 0 (Success)
+*/
+int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return 0;
-
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
