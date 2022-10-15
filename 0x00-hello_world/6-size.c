2712474c933ef82624105eb564fdfc72cf5a1103
Ukamaka Egwu <egwuukamaka@gmail.com>
2022-10-14 20:29:48 -0700

    C programming

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,20 +1,18 @@
 #include <stdio.h>
 /**
- * variables
- * mian - Entry Point
- * Return: Always 0 (Success)
- */
+* main - Entry Point
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
-	//Variables
-	char size = 1 bytes(s);
-	int size = 4 byte(s);
-	int long size = 4 byte(s);
-	int long long size = 8 byte(s);
-	float size = 4 byte(s);
-printf("Size of a char %c\n": size);
-printf("Size of an int %d\n": long size);
-printf("Size of an int %d\n": long long size);
-printf("Size of a float %f": size);
+char size = 1 bytes(s);
+int size = 4 byte(s);
+int long size = 4 byte(s);
+int long long size = 8 byte(s);
+float size = 4 byte(s);
+printf("Size of a char : %c\n" size);
+printf("Size of an int : %d\n" long size);
+printf("Size of an int : %d\n" long long size);
+printf("Size of a float : %f" size);
 return (0);
 }
