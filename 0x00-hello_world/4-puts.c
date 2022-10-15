360c8e8f61b14e109e966d94421c15b98eb334f7
Ukamaka Egwu <egwuukamaka@gmail.com>
2022-10-14 17:30:30 -0700

    C programming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 /**
- * Variables
  * Main - Entry Point
- * Return - 0 if true
+ *
+ * Return: Always 0 (Success)
  */
 
-int main()
+int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
-        return (0);
+puts("\"Programming is like building a multilingual puzzle\n");
+return (0);
 }
