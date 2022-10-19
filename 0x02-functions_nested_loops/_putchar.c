c17003c254ce6717c6e4d2d4fd1f17e2db4c99ca
Ukamaka Egwu <egwuukamaka@gmail.com>
2022-10-18 19:21:59 -0700

    nested loops

---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * _putcar -writes the character c to stdout
+ * @c: the character to print
+ *
+ * Return: 1 (success)
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
