e2699f7b797504458a6e98b69005ae7c6248c825
Ukamaka Egwu <egwuukamaka@gmail.com>
2022-10-21 10:27:57 -0700

    2-multiplication

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,14 +1,13 @@
-#include "main.h"
+#include <ctype.h>
 
 /**
- * _isdigit - checks if a char is a digit
- * @c: int to be checked
- * Return: 1 if c is a digit, 0 otherwise
+ * _isdigit - check if character is a digit
+ * @c: a character
+ * Return: 1 if @c is a digit, otherwise 0
  */
-int _isdigit(int c)
+int _isdigit(char c)
 {
-	int y = 0;
-	if (c >= '0' && c <= '9')
-		y = 1;
-	return (y);
+	if (isdigit(c))
+		return (1);
+	return (0);
 }
