fc738aa1dbc62629f70d6ebb8191c241882b617a
Ukamaka Egwu <egwuukamaka@gmail.com>
2022-11-14 20:16:20 -0800

    preprocessor

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/** Main - Entry point
+* Description: prints the name of the file it was
+*
+*                compiled from, followed by a new line
+* Return: 0
+*/
+
+int main(void)
+{
+	printf("%s\n", _file_);
+
+	return(0);
+}
