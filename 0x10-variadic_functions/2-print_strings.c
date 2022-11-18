a18e0cc599fbe7796d0870dae78edf0c2de82c16
Ukamaka Egwu <egwuukamaka@gmail.com>
2022-11-17 20:17:47 -0800

    variadic tasks

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -17,7 +17,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 
 	va_list args;
 
-	va_start(args, n)
+	va_start(args, n);
 	for (i = 0; i < n; i++)
 	{
 		j = va_arg(args, char *);
